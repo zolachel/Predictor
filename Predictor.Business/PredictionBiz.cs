292c5fe856b60ec223ffabbbf05744be9e33fa99
zolachel <zolachel@gmail.com>
2016-06-19 01:31:38 +0700

    adjust prediction page

--- Predictor.Business/PredictionBiz.cs
+++ Predictor.Business/PredictionBiz.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Predictor.DataAccess;
 using Predictor.Model;
+using System.Data.Entity;
 
 namespace Predictor.Business
 {
@@ -18,35 +19,49 @@ namespace Predictor.Business
 
         public List<PredictScoreModel> GetPredictableMatchs(string userId)
         {
-            List<PredictScoreModel> result = (_dbContext.Matches
-                                                       .Where(m => m.MatchStartTime > DateTime.Now && m.Tournament.IsActive)
-                                                       .Select(m => new PredictScoreModel() {
-                                                           TournamentName = m.Tournament.Name,
-                                                           NationCodeHome = m.Nation1.Code,
-                                                           NationNameHome = m.Nation1.Name,
-                                                           NationFlagHome = m.Nation1.FlagClass,
-                                                           NationCodeAway = m.Nation.Code,
-                                                           NationNameAway = m.Nation.Name,
-                                                           NationFlagAway = m.Nation.FlagClass,
-                                                           MatchId = m.Id,
-                                                           MatchStartTime = m.MatchStartTime,
-                                                           Remark = m.Remark
-                                                       })).ToList();
+            List<PredictScoreModel> result = new List<PredictScoreModel>();
 
-            List<Prediction> predictions = _dbContext.Predictions.Where(p => p.UserId == userId && p.Match.MatchStartTime > DateTime.Now).ToList();
+            DateTime noonToday = DateTime.Today.AddHours(12);
 
-            for (int i = 0; i < result.Count; i++) {
-                Prediction predicted = predictions.Where(p => p.MatchId == result[i].MatchId).FirstOrDefault();
+            IQueryable<DateTime> availableMatchTime = _dbContext.Matches
+                                                                .Where(m => m.MatchStartTime >= noonToday && m.Tournament.IsActive)
+                                                                .Select(m => m.MatchStartTime);
+            if (availableMatchTime.Count() > 0) {
+                DateTime cutOffTime = availableMatchTime.Min();
 
-                if (predicted != null) {
-                    result[i].ScoreHome = predicted.ScoreHome;
-                    result[i].ScoreAway = predicted.ScoreAway;
-                    result[i].UseMissile = predicted.UseMissile;
-                    result[i].Comment = predicted.Comment;
+                DateTime predictableTime = DateTime.Now < cutOffTime ? DateTime.Now : cutOffTime.AddDays(1).Date.AddHours(12);
+
+                result = (_dbContext.Matches
+                                    .Where(m => m.MatchStartTime > predictableTime && m.Tournament.IsActive)
+                                    .Select(m => new PredictScoreModel() {
+                                        TournamentName = m.Tournament.Name,
+                                        NationCodeHome = m.Nation1.Code,
+                                        NationNameHome = m.Nation1.Name,
+                                        NationFlagHome = m.Nation1.FlagClass,
+                                        NationCodeAway = m.Nation.Code,
+                                        NationNameAway = m.Nation.Name,
+                                        NationFlagAway = m.Nation.FlagClass,
+                                        MatchId = m.Id,
+                                        MatchStartTime = m.MatchStartTime,
+                                        Remark = m.Remark
+                                    })).ToList();
+
+                List<Prediction> predictions = _dbContext.Predictions.Where(p => p.UserId == userId && p.Match.MatchStartTime > predictableTime).ToList();
+
+                for (int i = 0; i < result.Count; i++) {
+                    Prediction predicted = predictions.Where(p => p.MatchId == result[i].MatchId).FirstOrDefault();
+
+                    if (predicted != null) {
+                        result[i].ScoreHome = predicted.ScoreHome;
+                        result[i].ScoreAway = predicted.ScoreAway;
+                        result[i].UseMissile = predicted.UseMissile;
+                        result[i].Comment = predicted.Comment;
+                    }
                 }
             }
 
             return result;
+
         }
     }
 }
--- Predictor.Web/Scripts/Views/Prediction/Predict.jsx
+++ Predictor.Web/Scripts/Views/Prediction/Predict.jsx
@@ -41,48 +41,63 @@ var PredictableMatch = React.createClass({
     handleCommentChange: function (e) {
         this.setState({ comment: e.target.value });
     },
+    handleSubmit: function (e) {
+        alert(this.props.matchId);
+    },
     render: function () {
         return (
             <div className="mdl-grid">
                 <div className="mdl-layout-spacer"></div>
-                <div className="mdl-cell mdl-cell--8-col mdl-cell--10-col-tablet mdl-cell--12-col-phone mdl-card mdl-shadow--4dp">
+                <div className="mdl-cell mdl-cell--10-col mdl-cell--12-col-phone mdl-card mdl-shadow--4dp">
                     <div className="mdl-card__title">
                         <h2 className="mdl-card__title-text">{moment(this.props.matchStartTimeUTCString).format(DATETIME_FORMAT)}</h2>
                     </div>
                     <div className="mdl-card__menu">
                         <div className="mdl-layout-spacer"></div>
-                        <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--mini-fab mdl-button--colored">
+                        <button onClick={this.handleSubmit} className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--mini-fab mdl-button--colored">
                             <i className="material-icons">save</i>
                         </button>
                     </div>
-                    <dv className="mdl-card__supporting-text mdl-grid">
-                        <div className="mdl-cell mdl-cell--4-col align-right">
-                            <h5>{this.props.nationNameHome}<span className={this.props.nationFlagHome}></span></h5>
-                        </div>
-                        <div className="mdl-cell mdl-cell--1-col">
-                            <div className="mdl-textfield mdl-js-textfield">
-                                <input className="mdl-textfield__input score-textbox" type="text" maxLength="1" pattern="[0-9]" id={"score-home" + this.props.matchId}  value={this.state.scoreHome} onChange={this.handleScoreHomeChange} />
-                                <label className="mdl-textfield__label" for={"score-home" + this.props.matchId}>score</label>
-                                <span className="mdl-textfield__error">Input is not number</span>
+                    <dv className="mdl-card__supporting-text">
+                        <div className="mdl-grid">
+                            <div className="mdl-cell mdl-cell--4-col mdl-cell--5-col-tablet mdl-cell--3-col-phone align-right">
+                                <h5>{this.props.nationNameHome}<span className={this.props.nationFlagHome}></span></h5>
+                            </div>
+                            <div className="mdl-cell mdl-cell--1-col mdl-cell--3-col-tablet mdl-cell--1-col-phone">
+                                <div className="mdl-textfield mdl-js-textfield">
+                                    <input className="mdl-textfield__input score-textbox" type="text" maxLength="1" pattern="[0-9]" id={"score-home" + this.props.matchId} value={this.state.scoreHome} onChange={this.handleScoreHomeChange} />
+                                    <label className="mdl-textfield__label" for={"score-home" + this.props.matchId}>score</label>
+                                    <span className="mdl-textfield__error">Input is not number</span>
+                                </div>
+                            </div>
+                            <div className="mdl-cell mdl-cell--2-col mdl-cell--8-col-tablet mdl-cell--4-col-phone align-center">
+                                <div className="mdl-textfield mdl-js-textfield width-auto" title="missile">
+                                    <label className="mdl-switch mdl-js-switch mdl-js-ripple-effect" for={"missile" + this.props.matchId}>
+                                        <input type="checkbox" id={"missile" + this.props.matchId} className="mdl-switch__input" checked={this.state.useMissile} onChange={this.handleUseMissileChange} />
+                                        <span className="mdl-switch__label"></span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="mdl-cell mdl-cell--1-col mdl-cell--3-col-tablet mdl-cell--1-col-phone">
+                                <div className="mdl-textfield mdl-js-textfield">
+                                    <input className="mdl-textfield__input score-textbox" type="text" maxLength="1" pattern="[0-9]" id={"score-away" + this.props.matchId} value={this.state.scoreAway} onChange={this.handleScoreAwayChange} />
+                                    <label className="mdl-textfield__label" for={"score-away" + this.props.matchId}>score</label>
+                                    <span className="mdl-textfield__error">Input is not number</span>
+                                </div>
+                            </div>
+                            <div className="mdl-cell mdl-cell--4-col mdl-cell--5-col-tablet mdl-cell--3-col-phone">
+                                <h5><span className={this.props.nationFlagAway }></span>{this.props.nationNameAway}</h5>
                             </div>
                         </div>
-                        <div className="mdl-cell mdl-cell--2-col align-center">
-                            <div className="mdl-textfield mdl-js-textfield width-auto" title="missile">
-                                <label className="mdl-switch mdl-js-switch mdl-js-ripple-effect" for={"missile" + this.props.matchId}>
-                                    <input type="checkbox" id={"missile" + this.props.matchId} className="mdl-switch__input" checked={this.state.useMissile} onChange={this.handleUseMissileChange} />
-                                    <span className="mdl-switch__label"></span>
-                                </label>
+                        <div className="mdl-grid">
+                            <div className="mdl-layout-spacer"></div>
+                            <div className="mdl-cell mdl-cell--10-col mdl-cell--8-col-tablet mdl-cell--4-col-phone">
+                                <div className="mdl-textfield mdl-js-textfield width-100-percent">
+                                    <input className="mdl-textfield__input" type="text" id={"comment" + this.props.matchId} value={this.state.comment} onChange={this.handleCommentChange} />
+                                    <label className="mdl-textfield__label" for={"comment" + this.props.matchId}>comment</label>
+                                </div>
                             </div>
-                        </div>
-                        <div className="mdl-cell mdl-cell--1-col">
-                            <div className="mdl-textfield mdl-js-textfield">
-                                <input className="mdl-textfield__input score-textbox" type="text" maxLength="1" pattern="[0-9]" id={"score-away" + this.props.matchId} value={this.state.scoreAway} onChange={this.handleScoreAwayChange} />
-                                <label className="mdl-textfield__label" for={"score-away" + this.props.matchId}>score</label>
-                                <span className="mdl-textfield__error">Input is not number</span>
-                            </div>
-                        </div>
-                        <div className="mdl-cell mdl-cell--4-col">
-                            <h5><span className={this.props.nationFlagAway }></span>{this.props.nationNameAway}</h5>
+                            <div className="mdl-layout-spacer"></div>
                         </div>
                     </dv>
                 </div>
